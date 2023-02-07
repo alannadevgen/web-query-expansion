aa36fc26c54317de5ec97257e19af7aaabfd1117
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-07 20:32:57 +0100

    Found documents from index

--- query/query.py
+++ query/query.py
@@ -36,10 +36,26 @@ class Query:
         self.tokens = [word.lower() for word in text.split(' ') if word not in self.stop_words]        
 
     def find_token_in_document(self):
-        documents_id = set()
+        documents_ids = []
+        documents = {}
         for token in self.tokens:
             if token in self.index.keys():
                 for document_id in self.index[token].keys():
-                    if document_id not in documents_id:
-                        documents_id.add(int(document_id))
-        self.found_documents = sorted(documents_id)
+                    # if document_id not in documents_ids:
+                    documents_ids.append(document_id)
+                    documents[token] = self.index[token]
+        if documents:
+            self.found_documents = documents
+            print(self.found_documents)
+        # if no token found in index
+        else:
+            self.found_documents = None
+
+    def find_documents_from_index(self):
+        ranked_documents = {}
+        for token in self.found_documents:
+            for document in self.found_documents[token]:
+                if document in ranked_documents:
+                    ranked_documents[document] += self.found_documents[token][document]['count']
+                else:
+                    ranked_documents[document] = self.found_documents[token][document]['count']
